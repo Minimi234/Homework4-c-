185f1adc217801acffe5467609ca69282822e39c
Anisa <An1sa_jon@mail.ru>
2022-08-25 13:00:42 +0400

    Task 25

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int Stepen (int a, int b)
+{ int i=1; int  p = 1;
+ while(i<=b)
+ {
+   p=p*a; i++;
+ }
+return p;
+}
+Console.WriteLine("Input  number a: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input  number b: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("a в степени b =" + Stepen(n1, n2) );
