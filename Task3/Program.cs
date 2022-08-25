67b656440c9961f126b69a469771823b7b84b258
Anisa <An1sa_jon@mail.ru>
2022-08-25 13:23:59 +0400

    Task 29

---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] RandomMassiv(int[] collect)
+{
+int length = collect.Length;
+int i = 0;
+while (i < length)
+ {  
+     collect[i] = new Random().Next(1, 100);
+     i++;
+ }
+return collect;
+}
+void PrintArray(int[] col)
+
+{
+   int count = col.Length;
+   int position = 0;
+   while (position < count)
+   {
+    Console.Write(col[position] + "  ");
+    position++;
+   }
+}
+Console.WriteLine("Задайте длину массива!" );
+int dlin = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[dlin];
+int[]mas=RandomMassiv (array);
+PrintArray(mas);
