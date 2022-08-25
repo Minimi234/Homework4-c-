c877e928d04f7137d762efd8d43eb46e7cd1817e
Anisa <An1sa_jon@mail.ru>
2022-08-25 12:58:27 +0400

    Task 27

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int summaChisel(int number)
+{
+    string workingNumber = Convert.ToString(number);
+    int sum = 0;
+    for (int i = 0; i < workingNumber.Length; i++)
+    {
+        sum = sum + Convert.ToInt32(workingNumber[i].ToString());
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма чисел: " + summaChisel(userNumber));
